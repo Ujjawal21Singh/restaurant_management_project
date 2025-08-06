aff1c40fc291dde608117d1e82ed9ad86094aa15
Ujjawal21Singh <144591174+Ujjawal21Singh@users.noreply.github.com>
2025-08-06 14:55:43 +0530

    22205

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.comfig.irls import render
 # Create your views here.
+
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
+
+handler404 = custom_404
+
