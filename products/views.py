ddebbda7a5ec4ff321275aff4f93c0b478a7c97e
Ujjawal21Singh <144591174+Ujjawal21Singh@users.noreply.github.com>
2025-08-07 12:30:11 +0530

    23631

--- products/views.py
+++ products/views.py
@@ -24,3 +24,15 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def menu_item(request):
+        menu_item = [
+            {"name": "Margherita Pizza", 'price': 299},
+            {"name": "paneer Tikka", "price": 349},
+            {"name": "Veg briyani", "price": 199},
+            {"name": "Butter Chicken", "price": 399},
+            {"name": "Chocolava Cake", "price": 299}
+        ]
+
+        return render(request, 'menu_item', {'menu_item': menu_item})
+
