9f11fbd0bb91d0bdf043ce361ec50ecf80086cea
Ujjawal21Singh <144591174+Ujjawal21Singh@users.noreply.github.com>
2025-08-07 12:53:56 +0530

    23633

--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.comfig.irls import render
+from django.comfig.urls import render
+from django.view.generic import TemplateView
 # Create your views here.
 
 def custom_404(request, exception):
     return render(request, '404.html', status=404)
 
-handler404 = custom_404
+def ContactView(TemplateView):
+    return render(request, 'contact.html')
 
