4cc93c0807c4ab8511e1b102b4c328eb79e31ee4
Ujjawal21Singh <144591174+Ujjawal21Singh@users.noreply.github.com>
2025-08-08 15:32:03 +0530

    23634

--- account/models.py
+++ account/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class RestaurantInfo(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
