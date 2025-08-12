21408d6ef4be48b1c1b61912f9f55c1c13894f24
Ujjawal21Singh <144591174+Ujjawal21Singh@users.noreply.github.com>
2025-08-12 11:44:51 +0530

    30617

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.comfig.urls import render
 from django.view.generic import TemplateView
+from .models import Reservation
+from django.db import DatabaseError 
 # Create your views here.
 
 def custom_404(request, exception):
@@ -8,4 +10,15 @@ def custom_404(request, exception):
 
 def ContactView(TemplateView):
     return render(request, 'contact.html')
+
+def reservations_view(request):
+    try:
+        reservations = Reservation.objects.all()
+        return render(request, "reservations.html", {"reservations": reservations})
+
+    except DatabaseError as e:
+        return HttpResponse("Sorry, we are experiencing technical issues.", status=500)
+    
+    except Exception as e:
+        return HttpResponse("An unexpected error occured", status=500)
 
