acff1865cae13a9687a9bb3d4e50be844cff5a05
Ujjawal21Singh <144591174+Ujjawal21Singh@users.noreply.github.com>
2025-08-13 12:39:46 +0530

    31814

--- account/models.py
+++ account/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class RestaurantInfo(models.Model):
@@ -7,4 +7,14 @@ class RestaurantInfo(models.Model):
     phone_number = models.CharField(max_length=20)
 
     def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(USer, on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15, blank=True)
+
+    def __str__(Self):
         return self.name
