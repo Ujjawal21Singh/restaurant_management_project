06484ad942e32612cfac6bd9afa5e2064f2ca040
Ujjawal21Singh <144591174+Ujjawal21Singh@users.noreply.github.com>
2025-08-15 11:51:40 +0530

    31818

---
+++ menu/MenuItemSerializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import MenuItem
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = [
+            "id",
+            'name',
+            'description',
+            'price',
+            'catogery',
+            'available',
+            'created_at',
+        ]
