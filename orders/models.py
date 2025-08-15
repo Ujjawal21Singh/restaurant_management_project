da8054ea423f4fe4a968ae3cd9c49f4df9cb45f8
Ujjawal21Singh <144591174+Ujjawal21Singh@users.noreply.github.com>
2025-08-15 11:12:24 +0530

    31818

--- orders/models.py
+++ orders/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
-from menu.models import Menu
+
 # Create your models here.
+
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class MenuItem(models.Model):
+    catogery = models.ForeignKey(MenuCategory, on_delete=models.CASCADE, related_name="items")
+    name = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.price}"
 
 class Order(models.Model):
     STATUS_CHOICES = [
