351f088192465fe5f824ffc312fb69c582736dc5
Ujjawal21Singh <144591174+Ujjawal21Singh@users.noreply.github.com>
2025-08-18 12:19:02 +0530

    35022

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+def Location(models.Model):
+    address_line1= models.CharField(max_length=255, hrlp_text="street address, P.O. Box, company name, c/o")
+    address_line2 = models.CharField(max_length=255, blank=True, null=True, help_text="Apartment, suite, unit, building, floor, etc.")
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20)
+    countary = models.CharField(max_length=100, default="India")
+
+    class Meta:
+        verbose_name = "Location"
+        verbose_name_prural = "Locations"
+
+    def __str__(self):
+        return f"{self.addrss_line1}, {self,city}, {self.state} {self.zip_code}"
