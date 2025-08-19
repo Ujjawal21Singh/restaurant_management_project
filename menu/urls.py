334e6158ac99c2448009284a6b7c6e8c42da5b15
Ujjawal21Singh <144591174+Ujjawal21Singh@users.noreply.github.com>
2025-08-19 11:48:15 +0530

    35020

--- menu/urls.py
+++ menu/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from restaurant_management import settings
 urlpatterns = [
     path('', views.menu_list, name='menu_list'),
-]
+]+ static(setting.MEDIA_URL, document_root=settings.MEDIA_ROOT)
