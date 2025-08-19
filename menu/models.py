0484788dba4446d30ebd8161614035ac61a4a35c
Ujjawal21Singh <144591174+Ujjawal21Singh@users.noreply.github.com>
2025-08-19 11:45:35 +0530

    35020

--- menu/models.py
+++ menu/models.py
@@ -14,6 +14,7 @@ class MenuItem(models.Model):
     catogery = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='main')
     available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null = True)
     
     def __str__(self):
         return f"{self.name}- ${self.price}"
