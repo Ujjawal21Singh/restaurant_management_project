ad0d1ab425f1cc65cc363badd99f52c2769c3365
Ujjawal21Singh <144591174+Ujjawal21Singh@users.noreply.github.com>
2025-08-20 20:01:21 +0530

    36580

--- contact/form.py
+++ contact/form.py
@@ -1,3 +1,16 @@
 from django import forms
-from .models import 
-class ContactForm
+from django.core.mail import send_mail
+
+send_mail(
+    'Subject',
+    "write message",
+    'from@example.com',
+    ['restaurant@restaurant.com'],
+    fail_silently=False
+)
+
+
+class ContactForm(form.Form):
+    name = form.CharField(max_length=100, label="Your name")
+    email = send_mail
+    
