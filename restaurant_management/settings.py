ad4a5e8295c6ffeef9319c3e174de7e54cd4e45b
Ujjawal21Singh <144591174+Ujjawal21Singh@users.noreply.github.com>
2025-08-21 13:27:58 +0530

    36581

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -105,8 +105,10 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-MEDIA_ROOT = BASE_DIR/'media'
+import os
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR/'media')
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
