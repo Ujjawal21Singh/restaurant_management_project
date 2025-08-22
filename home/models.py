d8e9dedc72766a18cc690c5b019b06895a1ef895
Ujjawal21Singh <144591174+Ujjawal21Singh@users.noreply.github.com>
2025-08-22 17:58:25 +0530

    37022

--- home/models.py
+++ home/models.py
@@ -22,6 +22,7 @@ class Restaurant(models.Model):
     address = Location()
     opening_house = models.JSONField(default=dict)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
+    logo = models.ImageField(upload_to="restaurant_logo",blank=True, null=True)
 
     def __str__(self):
         return self.name
