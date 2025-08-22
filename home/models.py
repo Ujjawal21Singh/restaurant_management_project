ebe014860ff0b8b755adee4a5e805f8b020c3153
Ujjawal21Singh <144591174+Ujjawal21Singh@users.noreply.github.com>
2025-08-22 17:10:50 +0530

    37023

--- home/models.py
+++ home/models.py
@@ -21,6 +21,7 @@ class Restaurant(models.Model):
     name = models.CharField(max_length=200)
     address = Location()
     opening_house = models.JSONField(default=dict)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.name
